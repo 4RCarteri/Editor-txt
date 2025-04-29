e3575687755243a16a970a30417d6b4f8b8e43e8
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-04-29 15:42:31 -0300

    Add Manager class for file operation defaults
    Introduces a static `Manager` class in the `Editor_txt` namespace that provides default values and properties for file operations, including folder path, file name, and file extension. It also features a computed property to generate the full file path.

---
+++ Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor_txt
+{
+    public static class Manager
+    {
+        // Default values for file operations
+        public static string DefaultFolderPath => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\";
+        public static string DefaultFileName => "NewFile";
+        public static string DefaultFileExtension => ".txt";
+
+        public static string FolderPath { get; set; } = DefaultFolderPath;
+        public static string FileName { get; set; } = DefaultFileName;
+        public static string FileExtension { get; set; } = DefaultFileExtension;
+        public static string FilePath => FolderPath + FileName + FileExtension;
+    }
+}
